8e5f22558e6114811085eb3d86d6ea0fae97e652
Izabela <dabrowska.izabelaa@wp.pl>
2018-10-29 23:55:11 +0100

    Slideshow buttons and dots

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-    $('.slideshow').slick();
+    $('.slideshow').slick({
+        dots: true,
+        dotsClass: 'slideshow-dots',
+        nextArrow: '<button class="button-next"><div class="button-next_icon"></div></button>',
+        prevArrow: '<button class="button-prev"><div class="button-prev_icon"></div></button>'
+    });
 
     var links = document.querySelectorAll('.nav-menu li a');
     var selectedLink = document.querySelectorAll('.nav-menu li a.active')[0];
