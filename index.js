c6e0152c996ff9470a153035d8e2549f37abb91f
Izabela <dabrowska.izabelaa@wp.pl>
2018-11-05 00:32:37 +0100

    Changing text after slide change

--- index.js
+++ index.js
@@ -1,9 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    var currentSlideIndex = 0;
+    var sliderChangeTimer;
+    var sliderSpeed = 500;
+    var numberOfSlides = 3;
+
+    var welcomeTexts = [
+        'Welcome to BINO',
+        'Welcome To CINO',
+        'Welcome to DINO',
+    ];
+    var $welcomeElement = $('.welcome-content h1')[0];
+
     $('.slideshow').slick({
         dots: true,
         dotsClass: 'slideshow-dots',
         nextArrow: '<button class="button-next"><div class="button-next_icon"></div></button>',
-        prevArrow: '<button class="button-prev"><div class="button-prev_icon"></div></button>'
+        prevArrow: '<button class="button-prev"><div class="button-prev_icon"></div></button>',
+        speed: sliderSpeed
+    });
+
+
+    $('button.button-next').click(function (event) {
+        console.log('click-next');
+
+        if (sliderChangeTimer == null) {
+            sliderChangeTimer = setTimeout(function () {
+                currentSlideIndex++;
+                currentSlideIndex = currentSlideIndex % numberOfSlides;
+                console.log(currentSlideIndex);
+                $welcomeElement.innerHTML = welcomeTexts[currentSlideIndex];
+                sliderChangeTimer = null;
+            }, sliderSpeed);
+        }
+
+    });
+
+    $('button.button-prev').click(function (event) {
+        console.log('click-prev');
+
+        if (sliderChangeTimer == null) {
+            sliderChangeTimer = setTimeout(function () {
+                currentSlideIndex--;
+                if (currentSlideIndex < 0) {
+                    currentSlideIndex = currentSlideIndex + numberOfSlides;
+                }
+                console.log(currentSlideIndex);
+                $welcomeElement.innerHTML = welcomeTexts[currentSlideIndex];
+                sliderChangeTimer = null;
+            }, sliderSpeed);
+        }
+
+
     });
 
     var links = document.querySelectorAll('.nav-menu li a');
